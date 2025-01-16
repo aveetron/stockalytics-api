c876214bec4e3791a2c802cea6d2372be7061206
avee <aveechakra@gmail.com>
2025-01-17 01:30:13 +0600

    cors gloabally decleared

--- src/main.ts
+++ src/main.ts
@@ -2,24 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // details cors config
-  app.enableCors({
-    origin: true,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: [
-      'Origin',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-      'Authorization',
-      'Access-Control-Allow-Origin',
-    ],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app
     .listen(process.env.PORT || 3000)
     .then(() => {
